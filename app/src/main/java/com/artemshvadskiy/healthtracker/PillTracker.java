437682eb19ce14e45cacbcf25e25aa6f0127b539
Artem Shvadskiy <shpen72991@gmail.com>
2016-09-03 21:33:21 -0700

    Init repo

---
+++ app/src/main/java/com/artemshvadskiy/healthtracker/PillTracker.java
@@ -0,0 +1,13 @@
+package com.artemshvadskiy.healthtracker;
+
+public class PillTracker {
+    private String mName;
+
+    public PillTracker(String name) {
+        mName = name;
+    }
+
+    public void logEvent() {
+
+    }
+}
---
+++ app/src/main/java/com/artemshvadskiy/healthtracker/TrackingManager.java
@@ -0,0 +1,8 @@
+package com.artemshvadskiy.healthtracker;
+
+/**
+ * Created by Artem on 9/3/2016.
+ */
+public class TrackingManager {
+    private Hashmap<String, >
+}
